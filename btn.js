52f7785146af63ac70c4e725760340dae956886d
4Zepar <153680673+4Zepar@users.noreply.github.com>
2024-01-31 09:52:24 +0300

    Add files via upload

---
+++ btn.js
@@ -0,0 +1,12 @@
+let button = document.getElementById('toTop');
+
+const onScroll = () => {
+  const scroll = document.documentElement.scrollTop
+  if (scroll > 0){
+    button.classList.add('active');
+  }else{
+    button.classList.remove('active');
+  }
+}
+
+window.addEventListener('scroll', onScroll)
