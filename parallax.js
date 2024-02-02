78551022caed97898c55d7cc6c8d1b544fec08e4
Zepar <krazerlord@gmail.com>
2024-02-02 13:36:07 +0300

    pravki

--- parallax.js
+++ parallax.js
@@ -1,8 +1,10 @@
-let aside = document.getElementById('behind');
-let article = document.getElementById('front');
+let behind = document.getElementById('behind');
+let front = document.getElementById('front');
+let text = document.getElementById('text');
 
 window.addEventListener('scroll', function(){
     let value = window.scrollY;
     behind.style.top = value * 0.3 + 'px';
     front.style.top = value * 0 + 'px';
+    text.style.left = value * 1.3 + 'px';
 })
